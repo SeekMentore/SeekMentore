f57c2652fe0e9e6b887cc932f03082e058e77ba5
JigyasaTomar <jigs.kcs@gmail.com>
2018-05-02 02:54:20 +0530

    commit subscriptiondetails.js

---
+++ src/main/webapp/scripts/subscriptiondetails.js
@@ -0,0 +1,206 @@
+/**
+ * 
+ */
+var subscriptionListMap = new Object();
+subscriptionListMap.selectedGrid = 'none';
+function loadGrids() {
+	callWebservice('/rest/admin/displayNonContactedSubscriptions', null, showNonContactedTutorRecords);
+	callWebservice('/rest/admin/displayNonVerifiedSubscriptions', null, showNonVerifiedTutorRecords);
+	callWebservice('/rest/admin/displayVerifiedSubscriptions', null, showVerifiedTutorRecords);
+	callWebservice('/rest/admin/displayVerificationFailedSubscriptions', null, showVerificationFailedTutorRecords);
+	callWebservice('/rest/admin/displayToBeRecontactedSubscriptions', null, showToBeRecontactedTutorRecords);
+	callWebservice('/rest/admin/displaySelectedSubscriptions', null, showSelectedTutorRecords);
+	callWebservice('/rest/admin/displayRejectedSubscriptions', null, showRejectedTutorRecords);
+}
+
+function showNonContactedTutorRecords(response){
+  prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'non_contacted');
+}
+
+function showNonVerifiedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'non_verified');
+}
+
+function showVerifiedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'verified');
+}
+
+function showVerificationFailedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'verification_failed');
+}
+
+function showToBeRecontactedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'to_be_recontacted');
+}
+
+function showSelectedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'selected');
+}
+
+function showRejectedTutorRecords(response){
+	prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, 'rejected');
+}
+
+function createSubscriptionNavigatorObject(subscriptionListResponse) {
+	var obj =	{
+		currentIndex 	: 0,
+		subscriptionList		: subscriptionListResponse,
+		
+		getCurrentSubscribedRecord	: function() {
+			if (null != this.subscriptionList && this.subscriptionList.length > 0)
+				return this.subscriptionList[this.currentIndex];
+			else 
+				return null;
+		},
+		
+		previousSubscribedRecord		: function() {
+			if (this.currentIndex > 0) {
+				this.currentIndex--;
+			} else {
+				this.currentIndex = this.subscriptionList.length - 1;
+			}
+			return this.getCurrentSubscribedRecord();
+		},
+		
+		nextSubscribedRecord			: function() {
+			if (this.currentIndex < this.subscriptionList.length - 1) {
+				this.currentIndex++;
+			} else {
+				this.currentIndex = 0;
+			}
+			return this.getCurrentSubscribedRecord();
+		},
+		
+		getParticularSubscribedRecord	: function(recordNumber) {
+			if (recordNumber >= 0 && recordNumber < this.subscriptionList.length) {
+				this.currentIndex = recordNumber;
+				return this.getCurrentSubscribedRecord();
+			}
+			return null;
+		},
+		
+		getList : function() {
+			return this.subscriptionList;
+		},
+		
+		getRecordCount : function() {
+			if (null != this.subscriptionList && this.subscriptionList.length > 0)
+				return this.subscriptionList.length;
+			else 
+				return 0;
+		},
+		
+		getListItem : function(index) {
+			if (null != this.subscriptionList && this.subscriptionList.length > 0)
+				return this.subscriptionList[index];
+			else 
+				return null;
+		}
+	}
+	return obj;
+}
+
+function prepareHTMLToFillGridAndSetMapNavigatorObjectList(response, gridName) {
+	subscriptionListMap[gridName] = createSubscriptionNavigatorObject(response);
+	var subscriptionNavigatorObject = subscriptionListMap[gridName];
+	var html ='';
+	for (var i=0;i < subscriptionNavigatorObject.getRecordCount(); i++) {
+		var subscriptionObj = subscriptionNavigatorObject.getListItem(i);
+		html +='<tr>';
+		html += '<td><a href="javascript:void(0);" onclick="getParticularSubscribedRecord(\''+gridName+'\', '+i+')">'+showValue(subscriptionObj.firstName)+' '+showValue(subscriptionObj.lastName)+ '</a></td>';
+		html += '<td><a href="mailto:'+showValue(subscriptionObj.emailId)+'">'+showValue(subscriptionObj.emailId)+'</a></td>';
+		html += '<td><a href="tel:'+showValue(subscriptionObj.contactNumber)+'">'+showValue(subscriptionObj.contactNumber)+'</a></td>';
+		html +='</tr>';
+	}
+	$('#'+gridName+'-tutor-records').html(html);
+	$('#'+gridName+'-total-records').html('Total Records = ' + subscriptionNavigatorObject.getRecordCount());
+}
+
+function previousSubscribedRecord() {
+	openSubscribedRecord(subscriptionListMap[subscriptionListMap.selectedGrid].previousSubscribedRecord());
+}
+
+function nextSubscribedRecord() {
+	openSubscribedRecord(subscriptionListMap[subscriptionListMap.selectedGrid].nextSubscribedRecord());
+}
+
+function getParticularSubscribedRecord(gridName, recordNumber) {
+	subscriptionListMap.selectedGrid = gridName;
+	openSubscribedRecord(subscriptionListMap[subscriptionListMap.selectedGrid].getParticularSubscribedRecord(recordNumber));
+}
+
+function hideSubscriptionAllRecordsPage() {
+	$('#selected-record-div').removeClass('noscreen');
+	$('.action-section').addClass('noscreen');
+	$('#download-report-div').addClass('noscreen');
+	$('#non_contacted-all-records-div').addClass('noscreen');
+	$('#non_verified-all-records-div').addClass('noscreen');
+	$('#verified-all-records-div').addClass('noscreen');
+	$('#verification_failed-all-records-div').addClass('noscreen');
+	$('#to_be_recontacted-all-records-div').addClass('noscreen');
+	$('#selected-all-records-div').addClass('noscreen');
+	$('#rejected-all-records-div').addClass('noscreen');
+	$('#header-div').addClass('noscreen');
+	$('#'+tutorListMap.selectedGrid+'-action-section').removeClass('noscreen');
+	var obj = document.getElementById(subscriptionListMap.selectedGrid+'-action-section');
+	if (null != obj) {
+		$('#remarks-div').removeClass('noscreen');
+	} else {
+		$('#remarks-div').addClass('noscreen');
+	}
+}
+
+function showSubscriptionAllRecordsPage() {
+	$('#selected-record-div').addClass('noscreen');
+	$('#download-report-div').removeClass('noscreen');
+	$('#non_contacted-all-records-div').removeClass('noscreen');
+	$('#non_verified-all-records-div').removeClass('noscreen');
+	$('#verified-all-records-div').removeClass('noscreen');
+	$('#verification_failed-all-records-div').removeClass('noscreen');
+	$('#to_be_recontacted-all-records-div').removeClass('noscreen');
+	$('#selected-all-records-div').removeClass('noscreen');
+	$('#rejected-all-records-div').removeClass('noscreen');
+	$('#header-div').removeClass('noscreen');
+}
+
+function openSubscribedRecord(subscriptionObj) {
+	hideSubscribedAllRecordsPage();
+	if (null != subscriptionObj) {
+		$('#APPLICATION_DATE').html(showValue(subscriptionObj.applicationDate));
+		$('#APPLICATION_STATUS').html(showValue(subscriptionObj.applicationStatus));
+		$('#CONTACT_NUMBER').html('<a href="tel:'+showValue(subscriptionObj.contactNumber)+'">'+showValue(subscriptionObj.contactNumber)+'</a>');
+		$('#EMAIL_ID').html('<a href="mailto:'+showValue(subscriptionObj.emailId)+'">'+showValue(subscriptionObj.emailId)+'</a>');
+		$('#FIRST_NAME').html(showValue(subscriptionObj.firstName));
+		$('#LAST_NAME').html(showValue(subscriptionObj.lastName));
+		$('#STUDENT_GRADE').html(showValue(subscriptionObj.studentGrade));
+		$('#SUBJECTS').html(showValue(subscriptionObj.subjects));
+		$('#PREFERRED_TIME_TO_CALL').html(showValue(subscriptionObj.preferredTimeToCall));
+		$('#ADDITIONAL_DETAILS').html(showValue(subscriptionObj.additionalDetails));
+		$('#IS_CONTACTED').html(showValue(subscriptionObj.isContacted));
+		$('#WHO_CONTACTED').html(showValue(subscriptionObj.whoContacted));
+		$('#CONTACTED_DATE').html(showValue(subscriptionObj.contactedDate));
+		$('#CONTACTED_REMARKS').html(showValue(subscriptionObj.contactedRemarks));
+		$('#IS_AUTHENTICATION_VERIFIED').html(showValue(subscriptionObj.isAuthenticationVerified));
+		$('#WHO_VERIFIED').html(showValue(subscriptionObj.whoVerified));
+		$('#VERIFICATION_DATE').html(showValue(subscriptionObj.verificationDate));
+		$('#VERIFICATION_REMARKS').html(showValue(subscriptionObj.verificationRemarks));
+		$('#IS_TO_BE_RECONTACTED').html(showValue(subscriptionObj.isToBeRecontacted));
+		$('#WHO_SUGGESTED_FOR_RECONTACT').html(showValue(subscriptionObj.whoSuggestedForRecontact));
+		$('#SUGGESTION_DATE').html(showValue(subscriptionObj.suggestionDate));
+		$('#SUGGESTION_REMARKS').html(showValue(subscriptionObj.suggestionRemarks));
+		$('#WHO_RECONTACTED').html(showValue(subscriptionObj.whoRecontacted));
+		$('#RECONTACTED_DATE').html(showValue(subscriptionObj.recontactedDate));
+		$('#RECONTACTED_REMARKS').html(showValue(subscriptionObj.recontactedRemarks));
+		$('#IS_SELECTED').html(showValue(subscriptionObj.isSelected));
+		$('#WHO_SELECTED').html(showValue(subscriptionObj.whoSelected));
+		$('#SELECTION_DATE').html(showValue(subscriptionObj.selectionDate));
+		$('#SELECTION_REMARKS').html(showValue(subscriptionObj.selectionRemarks));
+		$('#IS_REJECTED').html(showValue(subscriptionObj.isRejected));
+		$('#WHO_REJECTED').html(showValue(subscriptionObj.whoRejected));
+		$('#REJECTION_DATE').html(showValue(subscriptionObj.rejectionDate));
+		$('#REJECTION_REMARKS').html(showValue(subscriptionObj.rejectionRemarks));
+		$('#RECORD_LAST_UPDATED').html(showValue(subscriptionObj.recordLastUpdated));
+		
+		replacePlaceHoldersForEmailPanel(showValue(subscriptionObj.emailId), showValue(subscriptionObj.firstName)+' '+showValue(subscriptionObj.lastName));
+	} 
+}
